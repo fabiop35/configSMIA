d23e6e82d1b3036b4a1657e120752e03d5d9553f
NAME <mail@gmail.com>
2023-11-14 13:40:29 -0500

    CNSIA: Configuration properties to the catalog service

---
+++ createProject.sh
@@ -0,0 +1,2 @@
+curl https://start.spring.io/starter.zip -o catalog-service-CNSIA.zip -d type=gradle-project -d dependencies=web,lombok,actuator -d groupId=cnsia -d description="CNSIA Catalog Service" -d bootVersion=2.7.11 -d packageName=com.cnsia.catalogservice -d artifactId=catalog-service -d name=catalog-service -d javaVersion=17
+
